ee9d83eba32a289b8f00f5fb822d333a000807e4
OddMagnet <mibruenen@gmail.com>
2020-08-11 18:56:28 +0200

    Clean template

--- ParaphraseTests/ParaphraseTests.swift
+++ ParaphraseTests/ParaphraseTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Paraphrase
 
 class ParaphraseTests: XCTestCase {
 
@@ -17,17 +18,5 @@ class ParaphraseTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
