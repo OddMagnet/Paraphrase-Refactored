097e1a98a4510f90cee034e441309e362e889fec
OddMagnet <mibruenen@gmail.com>
2020-08-11 19:06:38 +0200

    Add loading, success on Test 1

--- Paraphrase/Model/QuotesModel.swift
+++ Paraphrase/Model/QuotesModel.swift
@@ -16,6 +16,23 @@ struct QuotesModel {
     }
     
     init(testing: Bool = false) {
-        
+        // load our quote data
+        let defaults = UserDefaults.standard
+        let quoteData : Data
+
+        // Only try to load saved quotes when not testing
+        if !testing, let savedQuotes = defaults.data(forKey: "SavedQuotes") {
+            // we have saved quotes; use them
+            SwiftyBeaver.info("Loading saved quotes")
+            quoteData = savedQuotes
+        } else {
+            // no saved quotes; load the default initial quotes
+            SwiftyBeaver.info("No saved quotes")
+            let path = Bundle.main.url(forResource: "initial-quotes", withExtension: "json")!
+            quoteData = try! Data(contentsOf: path)
+        }
+
+        let decoder = JSONDecoder()
+        quotes = try! decoder.decode([Quote].self, from: quoteData)
     }
 }
