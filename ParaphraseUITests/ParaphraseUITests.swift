45e4675d79d9b58696fcb616ccf4aeb9c2f84cbd
OddMagnet <mibruenen@gmail.com>
2020-08-15 20:30:14 +0200

    Fix app instantiation location

--- ParaphraseUITests/ParaphraseUITests.swift
+++ ParaphraseUITests/ParaphraseUITests.swift
@@ -9,10 +9,10 @@
 import XCTest
 
 class ParaphraseUITests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
 
